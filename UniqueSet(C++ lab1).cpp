0de134d03809b2c96d7517c081e11979c7a3c24e
Григорий Петров <petrovgrigorij29@gmail.com>
2023-11-07 18:13:17 +0400

    adding tests in main for all methods, constructors and task

--- UniqueSet(C++ lab1).cpp
+++ UniqueSet(C++ lab1).cpp
@@ -357,3 +357,95 @@ std::ostream& operator<<(std::ostream& os, UniqueSet<T>& set) {
 	}
 	return os;
 }
+
+using namespace std;
+
+int main() {
+	int iarray1[] = { 1, 3, 2, 5, 6, 6 ,6,2,1 };//5
+	int iarray2[] = { 4, 5, 7, 8, 9 };//5
+	int iarray3[] = { 1, 2, 3, 4, 5, 3, 2 };//5
+	int iarray4[] = { 1, 2, 3, 4, 5, 6, 6, 1 };//6
+	float farray1[] = { 1.12, 2.33, 1.45, 5.76, 2.33, 2.33, 9.875 };//7
+	float farray2[] = { 1.34, 3.23, 1.45, 5.76, 8.34 };//5
+	string sarray[] = { "gregory", "uniqueset", "laptop", "gregory" };//4
+	pair<int, double> parray1[] = { make_pair(1, 1.12), make_pair(5, 3.12), make_pair(3, 7.12), make_pair(9, 4.12), make_pair(5, 3.12) };//5
+	pair<int, double> parray2[] = { make_pair(3, 1.12), make_pair(5, 3.12), make_pair(1, 1.12), make_pair(5, 7.12), make_pair(6, 8.88) };//5
+
+	auto iset1 = UniqueSet<int>(iarray1, 9);
+	auto iset2 = UniqueSet<int>(iarray2, 5);
+
+	auto iset3 = UniqueSet<int>(iarray3, 5);
+	auto iset4 = UniqueSet<int>(iarray4, 6);
+
+	auto fset1 = UniqueSet<float>(farray1, 7);
+	auto fset2 = UniqueSet<float>(farray2, 5);
+
+	auto sset1 = UniqueSet<string>(sarray, 4);
+
+	auto pset1 = UniqueSet<pair<int, double>>(parray1, 5);
+	auto pset2 = UniqueSet<pair<int, double>>(parray2, 5);
+
+	cout << "Set of int" << "\n";
+	cout << iset1 << "\n\n";
+	cout << "Set of float" << "\n";
+	cout << fset1 << "\n\n";
+	cout << "Set of string" << "\n";
+	cout << sset1 << "\n\n";
+	cout << "Set of pair" << "\n";
+	cout << pset1 << "\n\n";
+
+	cout << "Sum of sets(int):" << "\n";
+	cout << iset1 << " + " << iset2 << " = ";
+	auto sum = iset1 + iset2;
+	cout << sum;
+	cout << "\n\n";
+
+	cout << "Difference of sets(float):" << "\n";
+	cout << fset1 << " - " << fset2 << " = ";
+	auto dif = fset1 - fset2;
+	cout << dif;
+	cout << "\n\n";
+
+	cout << "+= and -= with string set:\n";
+	cout << sset1 << "\n";
+	sset1 += "samara";
+	sset1 -= "gregory";
+	cout << sset1;
+	cout << "\n\n";
+
+	cout << "Difference of sets(pair):" << "\n";
+	cout << pset1 << " - " << pset2 << " = ";
+	auto dif_pair = pset1 - pset2;
+	cout << dif_pair;
+	cout << "\n\n";
+
+	cout << "Intersection of sets(int):" << "\n";
+	cout << iset1 << ",  " << iset2 << " = ";
+	auto inter_int = iset1.intersection(iset2);
+	cout << inter_int;
+	cout << "\n\n";
+
+	cout << "Relation of sets(int):" << "\n";
+	cout << iset4 << ",  " << iset3 << endl;
+	int rel = iset4.sub_super_set(iset3);
+	switch (rel) {
+	case 0:
+		cout << "sets are equal";
+		break;
+	case 1:
+		cout << iset4 << "subset of" << iset3;
+		break;
+	case 2:
+		cout << iset4 << "superset of " << iset3;
+		break;
+	case -1:
+		cout << "no point of intersection";
+	};
+	cout << "\n\n";
+
+	cout << "random c-or: \n";
+	auto rset1 = UniqueSet<int>(10, 1, 100);
+	cout << rset1;
+
+	return 0;
+}
